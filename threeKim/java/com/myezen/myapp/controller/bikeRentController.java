2e1303c9e962a0f0051d802b342570013ffbedca
guncode4545 <guncode4545@gmail.com>
2023-05-24 09:15:19 +0900

    협업 파트 나누기

--- threeKim/java/com/myezen/myapp/controller/bikeRentController.java
+++ threeKim/java/com/myezen/myapp/controller/bikeRentController.java
@@ -12,28 +12,42 @@ public class bikeRentController {
 	
 	BikeRentService bs; //업캐스팅 부모만 지정
 	
-	@RequestMapping(value="/bikeRent.do")
-	public String bikeRent() {
-		
-		return "bikeRent/bikeRent";
-	}
 	
-	@RequestMapping(value="/bikeRentDetail.do")
-	public String bikeRentDetail() {
-		
-		return "bikeRent/bikeRentDetail";
-	}
-	
-	
+	/*관리자페이지 자전거 등록*/
 	@RequestMapping(value="/bikeRentWrite.do")
 	public String bikeRentWrite() {
 		
 		return "bikeRent/bikeRentWrite";
 	}
+	/*관리자페이지 자전거 고장/신고 내역*/
+	@RequestMapping(value="/bikeRentFaultList.do")
+	public String bikeRentFaultList() {
+		
+		return "bikeRent/bikeRentFaultList";
+	}
+	/*자전거 대여소 위치*/
+	@RequestMapping(value="/bikeRentLocation.do")
+	public String bikeRentLocation() {
+		
+		return "bikeRent/bikeRentLocation";
+	}
+	/*자전거 소개글*/ 
+	@RequestMapping(value="/bikeRentInfo.do")
+	public String bikeRentInfo() {
+		
+		return "bikeRent/bikeRentInfo";
+	}
+	/*----------------------------------------------*/
 	
-	
+	/*자전거 QR대여*/
 	@RequestMapping(value="/bikeRentQR.do")
 	public String bikeRentQR() {
+		
+		
+		
+		
+		
+		
 		
 		return "bikeRent/bikeRentQR";
 	}
@@ -43,40 +57,199 @@ public class bikeRentController {
 	
 	
 	
-	@RequestMapping(value="/bikeRentLocation.do")
-	public String bikeRentLocation() {
+	
+	/*자전거상세보기*/	
+	@RequestMapping(value="/bikeRentDetail.do")
+	public String bikeRentDetail() {
 		
-		return "bikeRent/bikeRentLocation";
+		
+		
+		
+		
+		
+		
+		return "bikeRent/bikeRentDetail";
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	/*이용중인내역*/
+	@RequestMapping(value="/bikeRentUseList.do")
+	public String bikeRentUseList() {
+		
+		
+		
+		
+		
+		
+		
+		return "bikeRent/bikeRentUseList";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*----------------------------------------------*/
+	
+	/*반납하기*/
+	@RequestMapping(value="/bikeRentReturn.do")
+	public String bikeRentReturn() {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		return "bikeRent/bikeRentReturn";
+	}
+
+	/*자전거 대여 이력*/
 	@RequestMapping(value="/bikeRentHistory.do")
 	public String bikeRentHistory() {
 		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		return "bikeRent/bikeRentHistory";
 	}
-
 	
+	/*자전거 고장/신고 접수*/
 	@RequestMapping(value="/bikeRentFault.do")
 	public String bikeRentFault() {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		return "bikeRent/bikeRentFault";
 	}
 	
-	@RequestMapping(value="/bikeRentFaultList.do")
-	public String bikeRentFaultList() {
-		
-		return "bikeRent/bikeRentFaultList";
-	}
-	
-	@RequestMapping(value="/bikeRentInfo.do")
-	public String bikeRentInfo() {
-		
-		return "bikeRent/bikeRentInfo";
-	}
-	
-	
-	
-	
-
 	
 	
 	
@@ -96,20 +269,6 @@ public class bikeRentController {
 	
 	
 	
-	
-	
-	//이용중인내역 !김건우
-	@RequestMapping(value="/bikeRentUseList.do")
-	public String bikeRentUseList() {
-		
-		return "bikeRent/bikeRentUseList";
-	}
-	//반납하기 !김건우
-	@RequestMapping(value="/bikeRentReturn.do")
-	public String bikeRentReturn() {
-		
-		return "bikeRent/bikeRentReturn";
-	}
 	
 	
 	
