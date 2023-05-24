7e3c3fe2c31845ce447f13c2f23d0d546b67b924
guncode4545 <guncode4545@gmail.com>
2023-05-24 16:55:05 +0900

    인터셉터 추가 servlet-context.xml

--- threeKim/java/com/myezen/myapp/interceptor/LoginInterceptor.java
+++ threeKim/java/com/myezen/myapp/interceptor/LoginInterceptor.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class loginInterceptor extends HandlerInterceptorAdapter {
+public class LoginInterceptor extends HandlerInterceptorAdapter {
 
 	@Override																							//담아서 화면까지 가져가는 용도
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
@@ -19,7 +19,7 @@ public class loginInterceptor extends HandlerInterceptorAdapter {
 			request.getSession().setAttribute("midx", midx);
 			request.getSession().setAttribute("memberName", memberName);
 			
-		}
+		} 
 	}
 	
 	@Override
