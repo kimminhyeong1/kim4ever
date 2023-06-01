305b8893605115f5a3790cec4f6528493242a4c5
threeKim <qudtn941200@naver.com>
2023-06-01 14:03:40 +0900

    휴대폰 인증 유틸

--- threeKim/java/com/myezen/myapp/controller/MyController.java
+++ threeKim/java/com/myezen/myapp/controller/MyController.java
@@ -1,14 +1,21 @@
 package com.myezen.myapp.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.myezen.myapp.service.MyService;
 import com.myezen.myapp.util.NaverCloudPlatformSmsSender;
 
+
+@Controller
 public class MyController {
 
 	 private final MyService myService;
 
+	 
+	 	@Autowired
 	    public MyController(MyService myService) {
 	        this.myService = myService;
 	    }
@@ -16,11 +23,12 @@ public class MyController {
 	
 	
 	
-	
+	 @ResponseBody
 	 @PostMapping("/sendVerificationCode")
 	    public void sendVerificationCode(@RequestParam("phoneNumber") String phoneNumber) {
-	        String verificationCode = myService.generateVerificationCode();
-	        NaverCloudPlatformSmsSender.sendVerificationCode(phoneNumber, verificationCode);
-	        // 휴대폰 인증 코드 발송 후 처리할 로직 작성
+		 String verificationCode = myService.generateVerificationCode(); // 인증 코드 생성 추가
+		 
+        NaverCloudPlatformSmsSender.sendVerificationCode(phoneNumber, verificationCode);
+        // 휴대폰 인증 코드 발송 후 처리할 로직 작성
 	    }
 }
--- threeKim/java/com/myezen/myapp/util/NaverCloudPlatformSmsSender.java
+++ threeKim/java/com/myezen/myapp/util/NaverCloudPlatformSmsSender.java
@@ -1,6 +1,13 @@
 package com.myezen.myapp.util;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -13,28 +20,27 @@ public class NaverCloudPlatformSmsSender {
 	private static final String ACCESS_KEY = "9ThdTfh2xB3GVeQQQQCz";
     private static final String SECRET_KEY = "2mioSxQ8Z8SWtyeB3lvFx9us5svK4eonzudb472T";
     private static final String SERVICE_ID = "ncp:sms:kr:308560238965:threekim";
-    private static final String API_URL = "https://sens.apigw.ntruss.com";
-    private static final String API_VERSION = "v1";
-    private static final String API_URL_SMS = API_URL + "/" + API_VERSION + "/sms";
+    private static final String API_URL = "https://sens.apigw.ntruss.com/sms/v2/services/" + SERVICE_ID + "/messages";
 
     public static void sendVerificationCode(String phoneNumber, String verificationCode) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("X-NCP-APIGW-API-KEY-ID", ACCESS_KEY);
-        headers.set("X-NCP-APIGW-API-KEY", SECRET_KEY);
+        headers.set("x-ncp-apigw-timestamp", String.valueOf(System.currentTimeMillis()));
+        headers.set("x-ncp-iam-access-key", ACCESS_KEY);
+        headers.set("x-ncp-apigw-signature-v2", generateSignature());
 
         Map<String, Object> body = new HashMap<>();
         body.put("type", "SMS");
         body.put("contentType", "COMM");
         body.put("countryCode", "82");
-        body.put("from", "01056309412");
+        body.put("from", "01056309412"); // 발신자 번호 설정
         body.put("to", phoneNumber);
         body.put("content", "Verification code: " + verificationCode);
 
         HttpEntity<Map<String, Object>> request = new HttpEntity<>(body, headers);
 
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.exchange(API_URL_SMS, HttpMethod.POST, request, String.class);
+        ResponseEntity<String> response = restTemplate.exchange(API_URL, HttpMethod.POST, request, String.class);
 
         if (response.getStatusCode().is2xxSuccessful()) {
             // SMS 전송 성공 시 처리할 로직
@@ -42,4 +48,37 @@ public class NaverCloudPlatformSmsSender {
             // SMS 전송 실패 시 처리할 로직
         }
     }
+    
+    private static String generateSignature() {
+        String method = "POST";
+        String timestamp = String.valueOf(System.currentTimeMillis());
+
+        String space = " ";                    // 공백
+        String newLine = "\n";                  // 줄바꿈
+        String url = "/sms/v2/services/" + SERVICE_ID + "/messages"; // URL
+
+        String message = new StringBuilder()
+                .append(method)
+                .append(space)
+                .append(url)
+                .append(newLine)
+                .append(timestamp)
+                .append(newLine)
+                .append(ACCESS_KEY)
+                .toString();
+
+        try {
+            SecretKeySpec signingKey = new SecretKeySpec(SECRET_KEY.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+            Mac mac = Mac.getInstance("HmacSHA256");
+            mac.init(signingKey);
+            byte[] rawHmac = mac.doFinal(message.getBytes(StandardCharsets.UTF_8));
+            String signature = Base64.getEncoder().encodeToString(rawHmac);
+            return signature;
+        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    
 }
