6c67d4adadc998a38aa6ad1e4e4de717336d1220
ji1kim <wldnjs0752@gmail.com>
2023-06-01 09:40:53 +0900

    게시판 페이징 삭제

--- threeKim/java/com/myezen/myapp/domain/Criteria.java
+++ threeKim/java/com/myezen/myapp/domain/Criteria.java
@@ -1,25 +1,33 @@
 package com.myezen.myapp.domain;
 
 public class Criteria {
-//페이지 번호를 넘기는 기능을 하기 위한 클래스
+	
 	private int page;
-	private int perPageNum; //화면에 출력되는 리스트 수
+	private int perPageNum;   
+
 	public Criteria(){
-		this.page=1;
-		this.perPageNum=5;
-	}
+		this.page = 1;
+		this.perPageNum = 20;
+	}	
 	
 	public int getPage() {
 		return page;
 	}
+
 	public void setPage(int page) {
 		this.page = page;
 	}
+
 	public int getPerPageNum() {
 		return perPageNum;
 	}
+
 	public void setPerPageNum(int perPageNum) {
 		this.perPageNum = perPageNum;
 	}
+
+	
+	
+	
 	
 }
