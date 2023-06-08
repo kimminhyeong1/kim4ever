bcea5a12ef4508221a0423fad46486771ed839c2
guncode4545 <guncode4545@gmail.com>
2023-06-08 17:12:28 +0900

    카카오톡 소셜 로그인 기능 구현

--- threeKim/java/com/myezen/myapp/service/MemberServiceImpl.java
+++ threeKim/java/com/myezen/myapp/service/MemberServiceImpl.java
@@ -1,5 +1,12 @@
 package com.myezen.myapp.service;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,6 +18,8 @@ import org.springframework.security.config.annotation.web.configurers.Expression
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.myezen.myapp.domain.MemberVo;
 import com.myezen.myapp.persistance.MemberService_Mapper;
 import com.myezen.myapp.util.MailHandler;
@@ -176,14 +185,148 @@ public class MemberServiceImpl implements MemberService {
 	}
 	//소셜 회원가입
 	@Override
-	public int socialMemberInsert(String memberId, String memberPwd, String memberName) {
-		int value = msm.socialMemberInsert(memberId,memberPwd,memberName);
+	public int googleMemberInsert(String memberId, String memberPwd, String memberName) {
+		int value = msm.googleMemberInsert(memberId,memberPwd,memberName);
 		return value;
 	}
 	//소셜 멤버 확인
 	@Override
 	public int socialMemberCheck(String memberId) {
 		int value = msm.socialMemberCheck(memberId);
+		return value;
+	}
+	@Override
+	public String getAccessToken(String authorize_code) throws Throwable {
+		
+		String kakaoClientId ="76703a8d13e15a9a7deb9a931b73de9e"; //REST API
+		String kakaoRedirectUri ="http://localhost:8080/myapp/member/login/oauth2/code/kakao.do";
+		String access_Token = "";
+		String refresh_Token = "";
+		String id_token = "";
+		String reqURL = "https://kauth.kakao.com/oauth/token";
+
+		try {
+			URL url = new URL(reqURL);
+
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			// POST 요청을 위해 기본값이 false인 setDoOutput을 true로
+			
+			conn.setRequestProperty("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+			conn.setRequestMethod("POST");
+			conn.setDoOutput(true);
+			// POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
+
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+			StringBuilder sb = new StringBuilder();
+			sb.append("grant_type=authorization_code");
+
+			sb.append("&client_id="+kakaoClientId); // REST_API키 본인이 발급받은 key 넣어주기
+			sb.append("&redirect_uri="+kakaoRedirectUri); // REDIRECT_URI 본인이 설정한 주소 넣어주기
+
+			sb.append("&code=" + authorize_code);
+			bw.write(sb.toString());
+			bw.flush();
+
+			// 결과 코드가 200이라면 성공
+			int responseCode = conn.getResponseCode();
+			System.out.println("responseCode : " + responseCode);
+
+			// 요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line = "";
+			String result = "";
+
+			while ((line = br.readLine()) != null) {
+				result += line;
+			}
+			System.out.println("response body : " + result);
+
+			// jackson objectmapper 객체 생성
+			ObjectMapper objectMapper = new ObjectMapper();
+			// JSON String -> Map
+			Map<String, Object> jsonMap = objectMapper.readValue(result, new TypeReference<Map<String, Object>>() {
+			});
+
+			access_Token = jsonMap.get("access_token").toString();
+			refresh_Token = jsonMap.get("refresh_token").toString();
+			id_token = jsonMap.get("id_token").toString(); //id_token
+
+			System.out.println("access_token : " + access_Token);
+			System.out.println("refresh_token : " + refresh_Token);
+			System.out.println("id_token : " + id_token);
+
+			br.close();
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return access_Token;
+		
+	}
+	@SuppressWarnings("unchecked")
+	@Override
+	public HashMap<String, Object> getUserInfo(String access_Token) throws Throwable {
+		// 요청하는 클라이언트마다 가진 정보가 다를 수 있기에 HashMap타입으로 선언
+				HashMap<String, Object> userInfo = new HashMap<String, Object>();
+				String reqURL = "https://kapi.kakao.com/v2/user/me";
+
+				try {
+					URL url = new URL(reqURL);
+					HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+					conn.setRequestMethod("GET");
+
+					// 요청에 필요한 Header에 포함될 내용
+					conn.setRequestProperty("Authorization", "Bearer " + access_Token);
+
+					int responseCode = conn.getResponseCode();
+					System.out.println("responseCode : " + responseCode);
+
+					BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+					String line = "";
+					String result = "";
+
+					while ((line = br.readLine()) != null) {
+						result += line;
+					}
+					System.out.println("response body : " + result);
+					System.out.println("result type" + result.getClass().getName()); // java.lang.String
+
+					try {
+						// jackson objectmapper 객체 생성
+						ObjectMapper objectMapper = new ObjectMapper();
+						// JSON String -> Map
+						Map<String, Object> jsonMap = objectMapper.readValue(result, new TypeReference<Map<String, Object>>() {
+						});
+
+						System.out.println(jsonMap.get("properties"));
+
+						Map<String, Object> properties = (Map<String, Object>) jsonMap.get("properties");
+						Map<String, Object> kakao_account = (Map<String, Object>) jsonMap.get("kakao_account");
+
+						// System.out.println(properties.get("nickname"));
+						// System.out.println(kakao_account.get("email"));
+
+						String nickname = properties.get("nickname").toString();
+						String email = kakao_account.get("email").toString();
+
+						userInfo.put("nickname", nickname);
+						userInfo.put("email", email);
+
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				return userInfo;
+	}
+	@Override
+	//카카오회원가입
+	public int kakaoMemberInsert(String memberId, String memberPwd, String memberName) {
+		int value = msm.kakaoMemberInsert(memberId,memberPwd,memberName);
 		return value;
 	}
 
