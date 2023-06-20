a4fe8ff8f62b54cc057ce42a0218545bd9b05c17
guncode4545 <guncode4545@gmail.com>
2023-06-20 09:53:52 +0900

    다시 수정

--- threeKim/java/com/myezen/myapp/service/GatheringServiceImpl.java
+++ threeKim/java/com/myezen/myapp/service/GatheringServiceImpl.java
@@ -71,13 +71,6 @@ public class GatheringServiceImpl implements GatheringService {
 		//String savedGImgPath = request.getSession().getServletContext().getRealPath("/resources/GImages");
         String savedGTImgPath = "D://threekim//threeKim//src//main//webapp//resources/GTImages";//모임대표이미지
         String savedGImgPath = "D://threekim//threeKim//src//main//webapp//resources/GImages";//모임이미지
-		//모임 대표 이미지 
-    	if (GTImg.isEmpty()) {
-            System.out.println(GTImg);
-        } else {
-        	String uploadedGTImgName  = UploadFileUtiles.uploadFile(savedGTImgPath, GTImg.getOriginalFilename(), GTImg.getBytes());
-        	gjv.setImageName(uploadedGTImgName);
-        }
 
 		/*모임생성*/
 		//1.모임정보생성
@@ -85,7 +78,13 @@ public class GatheringServiceImpl implements GatheringService {
         //2.모임생성
         value = gsm.gatheringCreate(gjv);
         //3.모임 대표이미지 넣기
-        value = gsm.gatheringGTInsert(gjv);
+        if (GTImg.isEmpty()) {
+        	System.out.println(GTImg);
+        } else {
+        	String uploadedGTImgName  = UploadFileUtiles.uploadFile(savedGTImgPath, GTImg.getOriginalFilename(), GTImg.getBytes());
+        	gjv.setImageName(uploadedGTImgName);
+        	value = gsm.gatheringGTInsert(gjv);
+        }
         //4. 모임 이미지들 넣기
         for (MultipartFile file : GImg) {
         	if (file.isEmpty()) {
